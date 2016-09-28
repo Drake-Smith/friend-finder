c070b8f98d0c707da04087bb5b19785321a91153
Drake Smith <drakesmithjr@gmail.com>
2016-09-28 04:38:41 -0700

    added error message

--- app/server.js
+++ app/server.js
@@ -14,9 +14,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//require("./app/routing/api-routes.js")(app);
+require("./app/routing/api-routes.js")(app);
 // require("./app/routing/html-routes.js")(app);
 
+//ERROR:
+//module.js:327
+//     throw err;
+//     ^
+
+// Error: Cannot find module './app/routing/api-routes.js'
+//     at Function.Module._resolveFilename (module.js:325:15)
+//     at Function.Module._load (module.js:276:25)
+//     at Module.require (module.js:353:17)
+//     at require (internal/module.js:12:17)
+//     at Object.<anonymous> (/Users/drakesmithjr/Desktop/bootcamp/Homework/friend-finder/app/server.js:17:1)
+//     at Module._compile (module.js:409:26)
+//     at Object.Module._extensions..js (module.js:416:10)
+//     at Module.load (module.js:343:32)
+//     at Function.Module._load (module.js:300:12)
+//     at Function.Module.runMain (module.js:441:10)
 
 //route for home page, what user first sees
 app.get('/', function(req, res){
