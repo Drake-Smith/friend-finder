3112b32712493ac8303ab59908dfef362d906e6d
Drake Smith <drakesmithjr@gmail.com>
2016-09-29 02:32:49 -0700

    some cleanups

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,30 +4,30 @@ var friendArray = [
 		photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/480198_4844367586635_515105057_n.jpg?oh=3bbe6467160f41636b436ee073c4b719&oe=5866A777",
 		scores: [
 		 1,
-		 1,
-		 1,
-		 1,
-		 1,
-		 1,
-		 1,
-		 1,
-		 1,
-		 1
+		 3,
+		 2,
+		 4,
+		 5,
+		 3,
+		 3,
+		 2,
+		 3,
+		 2
 		]
 	},
 	{
 		name: "Big Bird",
 	  	photo: "http://vignette1.wikia.nocookie.net/muppet/images/9/92/Bigbirdnewversion.png/revision/latest?cb=20120128201030",
 	  	scores: [
+			4,
+			3,
+			3,
+			2,
+			5,
+			3,
 			1,
-			1,
-			1,
-			1,
-			1,
-			1,
-			1,
-			1,
-			1,
+			2,
+			4,
 			1
 		]
 	},
@@ -46,6 +46,54 @@ var friendArray = [
 			2,
 			4
 		]
+	},
+	{
+		name: "The creepy brother from 'Wedding Crashers'",
+	  	photo: "http://i50.photobucket.com/albums/f308/heart_of_silver/CinemaTech/Comedy/todd.jpg",
+	  	scores: [
+			2,
+			2,
+			4,
+			4,
+			3,
+			3,
+			1,
+			3,
+			2,
+			3
+		]
+	}, 
+	{
+		name: "Daniel Radcliffe, Dog Walker",
+	  	photo: "https://img.buzzfeed.com/buzzfeed-static/static/2014-07/1/10/enhanced/webdr09/enhanced-11477-1404224921-27.jpg",
+	  	scores: [
+			3,
+			5,
+			4,
+			1,
+			2,
+			1,
+			1,
+			5,
+			4,
+			5
+		]
+	},
+	{
+		name: "The Suh Dude",
+		photo: "http://www.thissongslaps.com/wp-content/uploads/2016/02/artworks-000138786392-3da84t-t500x500-e1455643663450.jpg",
+		scores: [
+			2,
+			1,
+			3,
+			5,
+			5,
+			4,
+			2,
+			5,
+			3,
+			1
+		]	
 	}
 ];
 
--- app/public/logic.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function(){
-
-	
-// 	$('#submit').on('click', function(){
-// 		alert("Submit bitch");
-// 	});
-// });
